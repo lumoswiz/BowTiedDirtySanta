bd2cf69cbf0017c425acf9ebb29db15759731ef7
lumoswiz <lumoswiz@protonmail.com>
2022-12-03 15:27:47 +1100

    added: steal mechanics, changed: OZ ERC721 private mappings to internal

--- src/DirtySanta.sol
+++ src/DirtySanta.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0;
 
-import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "./ERC721.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
 import {GDA} from "./GDA.sol";
@@ -10,6 +10,16 @@ import {toDaysWadUnsafe, toWadUnsafe} from "solmate/utils/SignedWadMath.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
 contract DirtySanta is ERC721, GDA, Ownable {
+    /// -----------------------------------------------------------------------
+    /// Events
+    /// -----------------------------------------------------------------------
+    event Stolen(
+        address indexed thief,
+        uint256 indexed tokenId,
+        uint256 indexed targetTokenId,
+        address victim
+    );
+
     /// -----------------------------------------------------------------------
     /// Errors
     /// -----------------------------------------------------------------------
@@ -20,6 +30,12 @@ contract DirtySanta is ERC721, GDA, Ownable {
     error DirtySanta__CannotExceedWalletLimit();
 
     error DirtySanta__SwapsAndMintingDisabled();
+
+    error DirtySanta__TargetTokenIdDoesNotExist();
+
+    error DirtySanta__CannotBeStolen();
+
+    error DirtySanta__NotNFTOwner();
 
     /// -----------------------------------------------------------------------
     /// Constants
@@ -34,6 +50,13 @@ contract DirtySanta is ERC721, GDA, Ownable {
     uint256 public totalSold = 0;
 
     uint256 public immutable startTime = block.timestamp;
+
+    /// -----------------------------------------------------------------------
+    /// Steal storage
+    /// -----------------------------------------------------------------------
+
+    /// @dev tokenId => number of times stolen
+    mapping(uint256 => uint256) public stolenCount;
 
     /// -----------------------------------------------------------------------
     /// Constructor
@@ -67,8 +90,38 @@ contract DirtySanta is ERC721, GDA, Ownable {
     }
 
     /// -----------------------------------------------------------------------
-    /// Transfer logic
+    /// Steal logic
     /// -----------------------------------------------------------------------
+
+    function steal(uint256 tokenId, uint256 targetTokenId) external {
+        /*=== Checks ===*/
+        if (block.timestamp >= 1671886800)
+            revert DirtySanta__SwapsAndMintingDisabled();
+
+        if (_exists(targetTokenId) != true)
+            revert DirtySanta__TargetTokenIdDoesNotExist();
+
+        if (ownerOf(tokenId) != msg.sender) revert DirtySanta__NotNFTOwner();
+
+        if (stolenCount[targetTokenId] == 2)
+            revert DirtySanta__CannotBeStolen();
+
+        /*=== Storage loads ===*/
+
+        address targetOwner = ownerOf(targetTokenId);
+
+        /*=== State updates ===*/
+
+        delete _tokenApprovals[tokenId];
+        delete _tokenApprovals[targetTokenId];
+
+        _owners[targetTokenId] = msg.sender;
+        _owners[tokenId] = targetOwner;
+
+        stolenCount[targetTokenId]++;
+
+        emit Stolen(msg.sender, tokenId, targetTokenId, targetOwner);
+    }
 
     /// -----------------------------------------------------------------------
     /// Overrides
--- test/DirtySanta.t.sol
+++ test/DirtySanta.t.sol
@@ -18,9 +18,13 @@ contract DirtySantaTest is Test {
 
     address alice = makeAddr("alice");
     address bob = makeAddr("bob");
+    address charlee = makeAddr("charlee");
 
     function setUp() public {
         vm.warp(1669961536);
+
+        vm.deal(bob, 1000 ether);
+        vm.deal(charlee, 1000 ether);
 
         nft = new DirtySanta(INITIAL_PRICE, SCALE_FACTOR, DECAY_CONSTANT);
     }
@@ -66,9 +70,8 @@ contract DirtySantaTest is Test {
     function test_cannotReceiveNFTWhenAlreadyOwnOne() public {
         startHoax(alice);
         nft.mint{value: uint256(INITIAL_PRICE)}();
-        vm.stopPrank();
 
-        startHoax(bob);
+        changePrank(bob);
 
         uint256 price = nft.getGDAPrice(
             toDaysWadUnsafe(block.timestamp - nft.startTime()),
@@ -78,5 +81,140 @@ contract DirtySantaTest is Test {
 
         vm.expectRevert();
         nft.safeTransferFrom(bob, alice, tokenId);
+    }
+
+    function test_steal() public {
+        startHoax(alice);
+        uint256 targetTokenId = nft.mint{value: uint256(INITIAL_PRICE)}();
+
+        assertEq(nft.balanceOf(alice), 1, "alice balance not 1");
+        assertEq(nft.ownerOf(targetTokenId), alice);
+
+        changePrank(bob);
+        uint256 price = nft.getGDAPrice(
+            toDaysWadUnsafe(block.timestamp - nft.startTime()),
+            nft.totalSold() + 1
+        );
+
+        uint256 tokenId = nft.mint{value: price}();
+
+        assertEq(nft.balanceOf(bob), 1, "bob balance not 1");
+        assertEq(nft.ownerOf(tokenId), bob);
+
+        nft.steal(tokenId, targetTokenId);
+
+        assertEq(nft.balanceOf(alice), 1, "alice balance not 1");
+        assertEq(nft.balanceOf(bob), 1, "bob balance not 1");
+
+        // token IDs should be swapped
+        assertEq(nft.ownerOf(tokenId), alice); // before: alice owned targetTokenId
+        assertEq(nft.ownerOf(targetTokenId), bob); // before: bob owned tokenId
+
+        // approvals should be reset
+        assertEq(
+            nft.getApproved(tokenId),
+            address(0),
+            "tokenId approval not reset"
+        );
+        assertEq(
+            nft.getApproved(targetTokenId),
+            address(0),
+            "targetTokenId approval not reset"
+        );
+
+        // stolen count should increment once for targetTokenId
+        assertEq(
+            nft.stolenCount(targetTokenId),
+            1,
+            "targetTokenId stolenCount not 1"
+        );
+
+        assertEq(nft.stolenCount(tokenId), 0, "tokenId stolenCount not 0");
+    }
+
+    function test_cannotStealBeyondChristmas() public {
+        startHoax(alice);
+        uint256 targetTokenId = nft.mint{value: uint256(INITIAL_PRICE)}();
+
+        changePrank(bob);
+        uint256 price = nft.getGDAPrice(
+            toDaysWadUnsafe(block.timestamp - nft.startTime()),
+            nft.totalSold() + 1
+        );
+
+        uint256 tokenId = nft.mint{value: price}();
+
+        vm.warp(1671886800);
+
+        vm.expectRevert(
+            DirtySanta.DirtySanta__SwapsAndMintingDisabled.selector
+        );
+        nft.steal(tokenId, targetTokenId);
+    }
+
+    function test_cannotStealTokenIdThatDoesNotExist() public {
+        startHoax(alice);
+        uint256 tokenId = nft.mint{value: uint256(INITIAL_PRICE)}();
+
+        vm.expectRevert(
+            DirtySanta.DirtySanta__TargetTokenIdDoesNotExist.selector
+        );
+        nft.steal(tokenId, 1);
+    }
+
+    function test_cannotStealWithTokenIdNotOwned() public {
+        startHoax(alice);
+        uint256 targetTokenId = nft.mint{value: uint256(INITIAL_PRICE)}();
+
+        changePrank(bob);
+        uint256 price = nft.getGDAPrice(
+            toDaysWadUnsafe(block.timestamp - nft.startTime()),
+            nft.totalSold() + 1
+        );
+        uint256 tokenId = nft.mint{value: price}();
+
+        changePrank(charlee);
+        vm.expectRevert(DirtySanta.DirtySanta__NotNFTOwner.selector);
+        nft.steal(tokenId, targetTokenId);
+    }
+
+    function test_cannotBeStolenMoreThanTwice() public {
+        startHoax(alice);
+        uint256 tokenIdA = nft.mint{value: uint256(INITIAL_PRICE)}();
+
+        changePrank(bob);
+        uint256 price = nft.getGDAPrice(
+            toDaysWadUnsafe(block.timestamp - nft.startTime()),
+            nft.totalSold() + 1
+        );
+        uint256 tokenIdB = nft.mint{value: price}();
+
+        changePrank(charlee);
+        price = nft.getGDAPrice(
+            toDaysWadUnsafe(block.timestamp - nft.startTime()),
+            nft.totalSold() + 1
+        );
+        uint256 tokenIdC = nft.mint{value: price}();
+
+        nft.steal(tokenIdC, tokenIdB);
+
+        assertEq(
+            nft.stolenCount(tokenIdB),
+            1,
+            "tokenIdB stolenCount not 1 after first steal"
+        );
+
+        changePrank(alice);
+        nft.steal(tokenIdA, tokenIdB);
+
+        assertEq(
+            nft.stolenCount(tokenIdB),
+            2,
+            "tokenIdB stolenCount not 2 after second steal"
+        );
+
+        changePrank(bob);
+        vm.expectRevert(DirtySanta.DirtySanta__CannotBeStolen.selector);
+        nft.steal(tokenIdC, tokenIdB);
     }
 }
